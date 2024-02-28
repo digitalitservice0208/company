cd8620f81676e296ff3805d94dbe853cd0e9c8dd
Josip <jp@cix.ie>
2024-02-28 05:27:25 +0100

    chore: rename the title

--- pages/_app.js
+++ pages/_app.js
@@ -19,7 +19,7 @@ export default class MyApp extends App {
         loading: true
     };
     componentDidMount() {
-        this.timerHandle = setTimeout(() => this.setState({ loading: false }), 2000); 
+        this.timerHandle = setTimeout(() => this.setState({ loading: false }), 2000);
     }
     componentWillUnmount() {
         if (this.timerHandle) {
@@ -27,7 +27,7 @@ export default class MyApp extends App {
             this.timerHandle = 0;
         }
     }
-    
+
     render () {
         const { Component, pageProps } = this.props
 
@@ -35,11 +35,11 @@ export default class MyApp extends App {
             <>
                 <Head>
                     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-                    <title>Pungent - React Multipurpose Startup & Digital Agency Template</title>
+                    <title>DITG - React Multipurpose Startup & Digital Agency Template</title>
                 </Head>
 
                 <Component {...pageProps} />
-                
+
                 {/* Preloader */}
                 <Loader loading={this.state.loading} />
 
